597cae03014c68f341f13c6dfb4159da385ef75a
papkon9690 <162000049+papkon9690@users.noreply.github.com>
2024-03-14 13:55:02 +0900

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -11,11 +11,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-import pandas as pd
-
 
 
 class Scraper:
